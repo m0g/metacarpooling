b1cf96ce63345342ec63fb29357595cdc4d56355
Loïc Nogues <nogues.loic@gmail.com>
2013-04-04 17:26:32 +0200

    added some stuffs

--- index.rb
+++ index.rb
@@ -1,11 +1,40 @@
 require 'sinatra'
 
+class Result
+  def initialize name, phone
+    @name = name
+    @phone = phone
+  end
+
+  def name
+    @name
+  end
+
+  def phone
+    @phone
+  end
+end
+
+class Search
+  def get_results
+    [
+      Result.new('Jean', '0450688799'),
+      Result.new('Pierre', '0450688708')
+    ]
+  end
+
+  def initialize search
+    @search = search
+  end
+end
+
 get '/' do
   erb :index
 end
 
 post '/' do
-  erb :results, locals: params
+  @results = Search.new(params[:search]).get_results
+  erb :results, locals: { results: @results }
 end
 
 not_found do
