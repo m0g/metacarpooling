9e02e6123f2993bb962839cde4629cd5ea3f1a55
Loïc Nogues <nogues.loic@gmail.com>
2013-04-30 12:47:24 +0200

    autocomplete for cities according to language & country

--- public/js/app.js
+++ public/js/app.js
@@ -10,14 +10,68 @@
 
   Form = {
 
+    autocompleteSource: function(request, response) {
+      var countryCode = $(this.element[0]).data('countrycode');
+      $.ajax({
+        url: "http://ws.geonames.org/searchJSON",
+        dataType: "jsonp",
+        data: {
+          featureClass: "P",
+          style: "full",
+          country: countryCode,
+          lang: $('html').attr('lang'),
+          maxRows: 12,
+          name_startsWith: request.term
+        },
+        success: function( data ) {
+          response( $.map( data.geonames, function( item ) {
+            return {
+              label: item.name + (item.adminName1 ? ", " + item.adminName1 : "") + ", " + item.countryName,
+              value: item.name
+            }
+          }));
+        }
+      });
+    },
+
+    autocompleteOpen: function() {
+      $( this ).removeClass( "ui-corner-all" ).addClass( "ui-corner-top" );
+    },
+
+    autocompleteClose: function() {
+      $( this ).removeClass( "ui-corner-top" ).addClass( "ui-corner-all" );
+    },
+
+    countryChange: function(e) {
+      var textInput = $(this).parent().children("input[type=\"text\"]");
+      var optionValue = $(this).find("option:selected").val();
+      var countryCode = $(this).find("option:selected").data('countrycode');
+
+      if (optionValue != '' && textInput.attr('disabled')){
+        textInput.removeAttr('disabled');
+      } else if (optionValue == '' && !textInput.attr('disabled')){
+        textInput.attr('disabled', true);
+      }
+
+      textInput.data('countrycode', countryCode);
+    },
+
     events: function() {
+      $('#from-city, #to-city').autocomplete({
+        source: Form.autocompleteSource,
+        minLength: 2,
+        open: Form.autocompleteOpen,
+        close: Form.autocompleteClose
+      });
+
+      $('#from-country, #to-country').change(Form.countryChange);
+
     },
 
     init: function() {
       this.events();
-      $("#when-date").datepicker({
-        dateFormat: "dd-mm-yy"
-      });
+      $('#from-city, #to-city').attr('disabled', true);
+      $("#when-date").datepicker({ dateFormat: "dd-mm-yy" });
     }
 
   }
