753c2032945b4b29dc324a1a198e1950ad24d430
Loïc Nogues <nogues.loic@gmail.com>
2013-08-07 11:58:34 +0200

    country preselected according to the current language

--- assets/js/metacarpooling.js
+++ assets/js/metacarpooling.js
@@ -80,10 +80,15 @@
 
     init: function() {
       this.events();
-      this.disableCityInputs();
 
-      $('#from-country option, #to-country option').filter(this.preSelectCountry)
-                                                   .prop('selected', true);
+      var select = $('#from-country option, #to-country option').filter(this.preSelectCountry);
+      console.log(select);
+
+      if (select.length > 0)
+        select.prop('selected', true);
+      else
+        this.disableCityInputs();
+
       $("#when-date").datepicker({ dateFormat: "dd-mm-yy" });
     }
 
